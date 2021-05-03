ad56268b0bb283bf8f6990b5403bcc2cdd9eca1d
Salma116 <s.elmourabet@gmail.com>
2021-05-03 10:40:13 +0200

    initialisation

---
+++ AsanoSurf/AsanoSurf/AsanoSurfApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsanoSurfApp.swift
+//  AsanoSurf
+//
+//  Created by Salma ElMourabet on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct AsanoSurfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsanoSurf/AsanoSurf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsanoSurf
+//
+//  Created by Salma ElMourabet on 03/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
